7b977d831659be84f5dd88051293fdeeb1ce3727
Emmanuelcrl12 <emmanuelrendon7848@gmail.com>
2025-05-27 10:39:21 -0500

    Add files via upload

---
+++ src/zonaCartas.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>]
+#include "Carta.h"
+using namespace std;
+
+class zonaCartas{
+public:
+    agregarcarta(Carta c);
+    Carta obtener(int n);
+
+
+
+
+
+
+
+};
+
+
+
+zonaCartas::zonaCartas()
+{
+    //ctor
+}
+
+zonaCartas::~zonaCartas()
+{
+    //dtor
+}
